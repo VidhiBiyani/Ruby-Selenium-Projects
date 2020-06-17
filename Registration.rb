3eccf71255789b72186dcd3b8a1a1e3e2545a9bd
Manisha-23Gupta <63473986+Manisha-23Gupta@users.noreply.github.com>
2020-06-17 17:36:56 +0530

    Update Registration.rb
    1. we are register in that site . 
    2.create an account
    3. entry all the  details in form.
    4. geting of text after register in the site.

--- Registration.rb
+++ Registration.rb
@@ -29,3 +29,37 @@ sleep 4
 driver.find_element(:id,"customer_firstname").send_keys "Manisha"
 driver.find_element(:id,"customer_lastname").send_keys "Gupta"
 sleep 5
+ driver.find_element(:id, "email").send_keys("manisha4567@gmail.com")
+  driver.find_element(:id, "passwd").send_keys("123456")
+  driver.find_element(:css, "select[id='days']").click
+  driver.find_element(:css, "select[id='days']").send_keys("1")
+  driver.find_element(:css, "select[id='months']").click
+  driver.find_element(:css, "select[id='months']").send_keys("december")
+  driver.find_element(:css, "select[id='years']").click
+  driver.find_element(:css, "select[id='years']").send_keys("1981")
+  driver.find_element(:id, "newsletter").click
+  driver.find_element(:id, "firstname").send_keys("")
+  driver.find_element(:id, "lastname").send_keys("")
+  driver.find_element(:id, "company").send_keys("EminenceSquareI")
+  driver.find_element(:name, "address1").send_keys("Scheme number 78")
+  driver.find_element(:name, "address2").send_keys("Vijay nagar 78")
+  driver.find_element(:id, "city").send_keys("hyderabad")
+  driver.find_element(:css, "select[id='id_state']").click
+  driver.find_element(:css, "select[id='id_state']").send_keys("indiana")
+  driver.find_element(:id, "postcode").send_keys("00002")
+  driver.find_element(:css, "select[id='id_country']").click
+  driver.find_element(:css, "select[id='id_country']").send_keys("united states")
+  driver.find_element(:id, "other").send_keys("hello i am here")
+  driver.find_element(:id, "phone").send_keys("90987654321")
+  driver.find_element(:name, "phone_mobile").send_keys("98907654321")
+  driver.find_element(:id, "alias").send_keys("L.I.G")
+  driver.find_element(:xpath, "//button[@name='submitAccount']").click
+  # Check that the checkbox exists
+   elementb = wait.until {
+    element = driver.find_element(:xpath, "//span[contains(text(),'Order history and details')]")
+    element if element.displayed?
+  }
+  puts "Test Passed: The element is displayed/exists"
+  elementb.click
+  sleep(5)
+end
